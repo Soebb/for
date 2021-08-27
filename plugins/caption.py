0af6dd3f1ef2ebd1e43887266b6c1662507474be
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-27 17:16:04 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -14,3 +14,13 @@ async def caption(client, message: Message):
             await message.edit("g")
         except Exception as e:
             print(e)
+
+
+@Client.on_message(filters.channel & filters.edited & (filters.video | filters.document))
+async def autopost(bot, update):
+    media = update.document or update.audio
+    if (update.chat.id == -1001264182630):
+        try:
+            await update.copy(chat_id=-1001448973320)
+        except Exception as error:
+            print(error)

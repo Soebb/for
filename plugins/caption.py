df6c94f50f26f47b11b0d3d084fe9ed5d6191ced
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-27 17:31:05 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
+from time import sleep
 
 chnls = "-1001166919373 -1001437520825 -1001071120514 -1001546442991 -1001322014891 -1001409508844"
 CHANNELS = set(int(x) for x in chnls.split())
@@ -11,10 +12,11 @@ async def caption(client, message: Message):
     media = message.document or message.audio
     if (media is not None) and (not message.chat.id in CHANNELS):
         try:
-            await message.edit("g")
+            Msg = await message.edit("g")
+            
             if (message.chat.id == -1001264182630):
                 try:
-                    await message.copy(chat_id=-1001448973320)
+                    await Msg.copy(chat_id=-1001448973320)
                 except Exception as error:
                     print(error)
         except Exception as e:
